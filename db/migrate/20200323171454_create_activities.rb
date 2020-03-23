14886f6f9d2ab1cc7ade0f7797e7d43d377976f2
SCDan0624 <dmayle1@hotmail.com>
2020-03-23 10:29:19 -0700

    Second commit

---
+++ db/migrate/20200323171454_create_activities.rb
@@ -0,0 +1,11 @@
+class CreateActivities < ActiveRecord::Migration[6.0]
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.string :duration
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
